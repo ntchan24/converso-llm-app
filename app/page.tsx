13972a2eb2943efc36dd16a758ab8b6591ffc4a3
Nathaniel Chan <nathaniel.chan530@gmail.com>
2025-07-20 16:00:04 -0600

    companionslist and cta finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,54 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
+import { recentSessions } from '@/constants'
 
 const Page = () => {
   return (
-    <div>
+    <main>
       <h1 className='text-2xl underline'>
-        Welcome to my SaaS App
+        Popular Companions
       </h1>
-      <Button>
-        let's get started
-      </Button>
-    </div>
+      <section className="home-section">
+        <CompanionCard 
+        id = "123"
+        name="Neura the Brainy Explorer"
+        topic="Neural Network of the Brain"
+        subject = "science"
+        duration={45}
+        color="#ffda6e"
+        />
+        <CompanionCard 
+        id = "456"
+        name="Countsy the Number Wizard"
+        topic="Derivatives and integrals"
+        subject = "maths"
+        duration={30}
+        color="#e5d0ff"
+        />
+        <CompanionCard 
+        id = "789"
+        name="Verba the Vocabulary Builder"
+        topic="langueage"
+        subject = "English literature"
+        duration={45}
+        color="#ffda6e"
+        />
+
+
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList 
+        title="Recently completed sessions"
+        companions={recentSessions} //from index.ts
+        classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
